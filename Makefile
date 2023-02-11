9079f69436d695cc34233545b1dc355c341ab234
Vasilii Morozov <i@vasilesk.ru>
2023-02-11 14:48:41 +0300

    mocks added

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 lint:
 	golangci-lint run -c .golangci.yml ./...
 
+generate:
+	go generate ./...
+
 test:
 	go test -count=1 ./... -covermode=atomic -v -race
 
@@ -19,5 +22,8 @@ docker-build-client:
 	docker build -t word-of-wisdom/client -f ./cmd/client/Dockerfile .
 
 docker-build-all: docker-build-wisdom docker-build-client
+
+install-mockery:
+	go install github.com/vektra/mockery/v2@v2.20.0
 
 .PHONY: lint test
--- pkg/pow/pow.go
+++ pkg/pow/pow.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 )
 
+//go:generate mockery --name ChallengeFactory
 type ChallengeFactory interface {
 	GetNewChallenge(ctx context.Context) (Challenge, error)
 	RestoreChallenge(_ context.Context, marshaled string) (Challenge, error)
 }
 
+//go:generate mockery --name Challenge
 type Challenge interface {
 	Check(ctx context.Context, solution Solution, data Data) (bool, error)
 	Solve(ctx context.Context, data Data) (Solution, error)
